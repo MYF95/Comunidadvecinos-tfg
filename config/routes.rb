00869e3a46f79763fff85869442e335112b6ff30
myf95 <myf1995@gmail.com>
2018-06-19 18:46:18 +0100

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root                    to: 'static_pages#home'
+  get '/help',            to: 'static_pages#help'
+  get '/about',           to: 'static_pages#about'
+end

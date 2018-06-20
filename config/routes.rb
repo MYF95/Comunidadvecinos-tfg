3b5bfe831f2121851faf26d0e6df36678604bc80
myf95 <myf1995@gmail.com>
2018-06-21 00:43:47 +0100

    added user model using devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
   root                    to: 'static_pages#home'
   get '/help',            to: 'static_pages#help'
   get '/about',           to: 'static_pages#about'
 
   resources :apartments
+  resources :users
 end

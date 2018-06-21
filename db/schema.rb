af4a8c4570e2b8d4a867e86c98d64b7e55a60c87
myf95 <myf1995@gmail.com>
2018-06-22 00:29:00 +0100

    añadido el atributo de administrador al usuario

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_230632) do
+ActiveRecord::Schema.define(version: 2018_06_21_231908) do
 
   create_table "apartments", force: :cascade do |t|
     t.string "owner"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_06_20_230632) do
     t.string "first_name"
     t.string "last_name"
     t.date "birthday"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

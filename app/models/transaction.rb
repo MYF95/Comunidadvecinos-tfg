85625a3929f232f8944132dcd7ddb7fc74f42c44
myf95 <myf1995@gmail.com>
2018-06-23 15:26:29 +0100

    añadido tests básicos de los movimientos

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,7 @@
 class Transaction < ApplicationRecord
+
+  validates :concept, presence: true
+  validates :date, presence: true
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+
 end

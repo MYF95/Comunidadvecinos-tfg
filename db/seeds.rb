41dbbd260a274d7cca09ba10a9cbd05100a2fb17
myf95 <myf1995@gmail.com>
2018-06-25 19:41:01 +0100

    añadido datos por defecto para viviendas con usuarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
 User.create!(email: 'admin@test.com', first_name: 'Admin', last_name: 'User', birthday: '27-06-2018', admin: true, password: 'chicken', password_confirmation: 'chicken')
 User.create!(email: 'user@test.com', first_name: 'Test', last_name: 'User', birthday: '27-06-2018', admin: false, password: 'chicken', password_confirmation: 'chicken')
 User.create!(email: 'ponsan@bork.com', first_name: 'Ponsan', last_name: 'Yao', birthday: '27-06-2018', admin: true, password: 'chicken', password_confirmation: 'chicken')
-User.create!(email: 'tsukki@test.com', first_name: 'Tsukki', last_name: 'Yao', birthday: '27-06-2018', admin: false, password: 'chicken', password_confirmation: 'chicken')
+User.create!(email: 'tsukki@bork.com', first_name: 'Tsukki', last_name: 'Yao', birthday: '27-06-2018', admin: false, password: 'chicken', password_confirmation: 'chicken')
 
 Apartment.create(owner: 'Ponsan', floor: 1, letter: 'A', fee: 50)
 Apartment.create(owner: 'Admin', floor: 1, letter: 'B', fee: 50)
+
+UserApartment.create(user_id: 1, apartment_id: 3)
+UserApartment.create(user_id: 3, apartment_id: 1)
+UserApartment.create(user_id: 4, apartment_id: 1)
 
 Statement.create(name: 'Extracto Junio 2018', date: '04-06-2018')
 

fd23d16c8f36d08315e385cdeea38036a9d898de
myf95 <myf1995@gmail.com>
2018-06-26 15:50:39 +0100

    arreglado los tests de los movimientos bancarios

--- test/controllers/movements_controller_test.rb
+++ test/controllers/movements_controller_test.rb
@@ -16,8 +16,8 @@ class MovementsControllerTest < ActionDispatch::IntegrationTest
   # FUNCTIONS
 
   def assert_permissions
-    assert_redirected_to root_path
-    assert_select 'div', 'Tu cuenta no tiene permisos para realizar esa acción. Por favor, contacta con el administrador para más información.'
+    follow_redirect!
+    assert_equal flash[:danger], 'Tu cuenta no tiene permisos para realizar esa acción. Por favor, contacta con el administrador para más información.'
   end
 
   # TESTS
@@ -38,74 +38,73 @@ class MovementsControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h1', "Datos del movimiento bancario #{@movement.concept}"
   end
 
-  # TODO Averiguar por qué los tests con usuarios no están funcionando
+  test 'create as non-admin user should redirect to homepage with message' do
+    log_in_as(@user)
+    get new_movement_path
+    post movements_path, params: { movement: { concept: @concept, date: @date, amount: '50', description: @description}}
+    assert_permissions
+  end
 
-  # test 'create as non-admin user should redirect to homepage with message' do
-  #   log_in_as(@user)
-  #   get new_transaction_path
-  #   post transactions_path, params: { @movement: { concept: @concept, date: @date, amount: '50', description: @description}}
-  #   assert_permissions
-  # end
-  #
-  # test 'edit as non-admin user should redirect to homepage with message' do
-  #   log_in_as(@user)
-  #   get edit_transaction_path(@@movement)
-  #   assert_template 'movements/edit'
-  #   post transactions_path, params: { @movement: { concept: @concept, date: @date, amount: '50', description: @description}}
-  #   assert_permissions
-  # end
-  #
-  # test 'delete as non-admin user should redirect to homepage with message' do
-  #   log_in_as(@user)
-  #   get transaction_path(@@movement)
-  #   assert_no_difference 'Movement.count' do
-  #     delete transaction_path(@@movement)
-  #   end
-  #   assert_permissions
-  # end
-  #
-  # test 'create @movement should work properly as admin user' do
-  #   log_in_as(@admin)
-  #   get new_transaction_path
-  #   assert_difference 'Movement.count', 1 do
-  #     post transactions_path, params: { @movement: { concept: @concept, date: @date, amount: '50', description: @description}}
-  #   end
-  #   follow_redirect!
-  #   assert_template 'movements/show'
-  #   assert_not flash.empty?
-  # end
-  #
-  # test 'update @movement should work properly as admin user with correct data' do
-  #   log_in_as(@admin)
-  #   get edit_transaction_path(@@movement)
-  #   binding.pry
-  #   put transactions_path, params: { @movement: { concept: 'New @movement', date: '28-06-2018', amount: '60', description: 'New description'}}
-  #   assert_not flash.empty?
-  #   assert_redirected_to @@movement
-  #   @@movement.reload
-  #   assert_equal 'New @movement', @@movement.concept
-  #   assert_equal '28-06-2018', @@movement.date
-  #   assert_equal '60', @@movement.amount
-  #   assert_equal 'New description', @@movement.description
-  # end
-  #
-  # test 'update @movement should not work as admin user with incorrect data' do
-  #   log_in_as(@admin)
-  #   get edit_transaction_path(@@movement)
-  #   post transactions_path, params: { @movement: { concept: '', date: '', amount: '', description: ''}}
-  #   assert_select 'div.alert', 'The form contains 4 errors.'
-  # end
-  #
-  # test 'destroy @movement should work properly as admin user' do
-  #   log_in_as(@admin)
-  #   get transaction_path(@@movement)
-  #   assert_template 'movements/show'
-  #   assert_difference 'Movement.count', -1 do
-  #     delete transaction_path(@@movement)
-  #   end
-  #   assert_redirected_to transactions_path
-  # end
-  #
+  test 'edit as non-admin user should redirect to homepage with message' do
+    log_in_as(@user)
+    get edit_movement_path(@movement)
+    assert_template 'movements/edit'
+    post movements_path, params: { movement: { concept: @concept, date: @date, amount: '50', description: @description}}
+    assert_permissions
+  end
+
+  test 'delete as non-admin user should redirect to homepage with message' do
+    log_in_as(@user)
+    get movement_path(@movement)
+    assert_no_difference 'Movement.count' do
+      delete movement_path(@movement)
+    end
+    assert_permissions
+  end
+
+  test 'create movement should work properly as admin user' do
+    log_in_as(@admin)
+    get new_movement_path
+    assert_difference 'Movement.count', 1 do
+      post movements_path, params: { movement: { concept: @concept, date: @date, amount: '50', description: @description}}
+    end
+    follow_redirect!
+    assert_template 'movements/show'
+    assert_not flash.empty?
+  end
+
+  test 'update movement should work properly as admin user with correct data' do
+    log_in_as(@admin)
+    get edit_movement_path(@movement)
+    patch movement_path(@movement), params: { movement: { concept: 'New movement', date: '28-06-2018', amount: '60', description: 'New description'}}
+    assert_not flash.empty?
+    assert_redirected_to @movement
+    @movement.reload
+    assert_equal 'New movement', @movement.concept
+    assert_equal '28-06-2018', @movement.date.strftime("%d-%m-%Y")
+    assert_equal 60, @movement.amount
+    assert_equal 'New description', @movement.description
+  end
+
+  test 'update movement should not work as admin user with incorrect data' do
+    log_in_as(@admin)
+    get edit_movement_path(@movement)
+    patch movement_path(@movement), params: { movement: { concept: '', date: '', amount: '', description: ''}}
+    assert_select 'div.alert', 'El formulario contiene algunos errores.'
+  end
+
+  test 'destroy movement should work properly as admin user' do
+    log_in_as(@admin)
+    get movement_path(@movement)
+    assert_template 'movements/show'
+    assert_difference 'Movement.count', -1 do
+      delete movement_path(@movement)
+    end
+    assert_redirected_to movements_path
+  end
+
+  #TODO tests para crear un extracto, dividir un movimiento
+
   # test 'create for statement' do
   # end
   #

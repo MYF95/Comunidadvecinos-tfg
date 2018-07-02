00da486daf33ef22c3dafa0cabe123f1140ea4ab
myf95 <myf1995@gmail.com>
2018-07-02 18:30:16 +0100

    añadido lógica de borrado de un extracto con respecto a movimientos

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,4 +1,6 @@
 class Statement < ApplicationRecord
+  before_destroy :destroy_movements
+
   has_many :statement_movements
   has_many :movements, through: :statement_movements
   has_one_attached :bank_statement
@@ -6,4 +8,15 @@ class Statement < ApplicationRecord
   validates :name, presence: true
   validates :date, presence: true
 
+  private
+    def destroy_movements
+      unless self.movements.empty?
+        self.movements.each do |movement|
+          StatementMovement.find_by(statement: self, movement: movement).destroy
+          unless movement.statements.count > 0
+            movement.destroy
+          end
+        end
+      end
+    end
 end

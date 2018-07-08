7996913a97965ae2a79e91a90c9696ea880bb85f
myf95 <myf1995@gmail.com>
2018-07-09 00:42:07 +0100

    added user model tests

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,5 +1,4 @@
 class Statement < ApplicationRecord
-  # before_destroy :destroy_movements
   before_destroy :destroy_movement_statements
   before_destroy :destroy_bank_statement
 
@@ -10,22 +9,6 @@ class Statement < ApplicationRecord
   validates :name, presence: true
 
   private
-    # def destroy_movements
-    #   unless self.statement_movements.empty?
-    #     self.statement_movements.each do |statement_movement|
-    #       movement = Movement.find(statement_movement.movement_id)
-    #       unless movement.statement_movements.count > 1
-    #         movement.destroy
-    #       end
-    #     end
-    #   end
-    # end
-    #
-    # def destroy_movement_statements
-    #   unless self.statement_movements.empty?
-    #     self.statement_movements.destroy_all
-    #   end
-    # end
 
     def destroy_movement_statements
       unless self.movements.empty?
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,13 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:user)
+  end
 
   test 'Users - destroy user should destroy association with apartments' do
-
+    assert_difference 'UserApartment.count', -1 do
+      assert_no_difference 'Apartment.count' do
+        @user.destroy
+      end
+    end
   end
 
   test 'Users - destroy user should destroy owner association with apartments' do
-
+    assert_difference 'ApartmentOwner.count', -1 do
+      assert_no_difference 'Apartment.count' do
+        @user.destroy
+      end
+    end
   end
 
 end
